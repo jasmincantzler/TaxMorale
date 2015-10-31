9f75976bf57b73c0fd367d3537d5817e2c80aa33
wwiebke <w-weiger@msn.com>
2015-10-31 17:10:32 +0100

    download data and write as csv
    As a preparatory step I downloaded all of our datasets individually
    again and exported them in csv-format.
    Note, that I have used "csv2" instead of normal "csv" because "csv2"
    works with German Excel (comma as a decimal point and semicolon as
    seperator) as compared to the standard version ("." for decimal point
    and comma as a seperator)

---
+++ Assignment 3/PreparatoryWork/downloading_and_exporting_Afrobarometer.R
@@ -0,0 +1,61 @@
+################################
+#  Loading Afrobarometer Data  #
+#  Wiebke Weiger               #
+#  31 October 2015             #
+# ##############################
+
+
+# set the working directory:
+setwd("C:/Users/Wiebke/Documents/RepRes/TaxMorale/Assignment 3/PreparatoryWork")
+# in this case really important, because it specifies where the csv-files are stored
+# unless you specify the file-path in the write.cvs command
+
+# packages used for this Rfile:
+library(rio) # needed to download data from the Afrobarometer website in SPSS format
+library(foreign) # needed to export data from R to other formats
+
+
+# 1) Download Afrobarometer data from website
+
+# round 5 of the Afrobarometer: 
+round5 <- rio::import("http://afrobarometer.org/sites/default/files/data/round-5/merged_r5_data_0.sav")
+
+# export data into csv format:
+write.csv2(round5, file="round5.csv", quote=FALSE, na="NA")
+# we use "write.csv2" here because this R-source file is run on a German computer
+# where Excel is programmed to recognize a comma as a decimal point and a 
+# semicolon for the seperator which "csv2" accounts for 
+# if you want to use it on a different computer with the American setting please
+# run:
+# write.csv(round5, file="round5.csv", quote=FALSE, na="NA") # which usese "." for
+# the decimal point and a comma for the seperator
+
+# export into Stata format:
+# write.dta
+
+
+
+# round 4 of the Afrobarometer: 
+round4 <- rio::import("http://afrobarometer.org/sites/default/files/data/round-4/merged_r4_data.sav")
+
+# export data into csv format:
+write.csv2(round4, file="round4.csv", quote=FALSE, na="NA")
+# write.csv(round4, file="round4.csv", quote=FALSE, na="NA")
+
+
+# round 3 of the Afrobarometer: 
+round3 <- rio::import("http://afrobarometer.org/sites/default/files/data/round-3/merged_r3_data.sav")
+
+# export data into csv format:
+write.csv2(round3, file="round3.csv", quote=FALSE, na="NA")
+# write.csv(round3, file="round3.csv", quote=FALSE, na="NA")
+
+
+# round 2 of the Afrobarometer:
+round2 <- rio::import("http://afrobarometer.org/sites/default/files/data/round-2/merged_r2_data.sav")
+
+# export data into csv format:
+write.csv2(round2, file="round2.csv", quote=FALSE, na="NA")
+# write.csv(round2, file="round2.csv", quote=FALSE, na="NA")
+
+
