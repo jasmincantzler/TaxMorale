aaee8777c04608749db85288627fbc07556db5b9
wwiebke <w-weiger@msn.com>
2015-11-01 13:39:24 +0100

    Included "load" command
    Since the "read.csv" command didn't really work, I included the "load"
    command which loads the previously saved RData files into the R
    workspace which is much faster than downloading them. Also this time,
    the number of observations and variables didn't change compared to what
    they were after the download

--- Assignment 3/PreparatoryWork/cleaning_data.R
+++ Assignment 3/PreparatoryWork/cleaning_data.R
@@ -9,15 +9,23 @@ setwd("C:/Users/Wiebke/Documents/RepRes/TaxMorale/Assignment 3/PreparatoryWork")
 
 # load the data into R from the previously created csv-files:
 
-round2 <- read.csv2("round2.csv", header=TRUE, sep = ";", quote = "\"",
+#round2 <- read.csv2("round2.csv", header=TRUE, sep = ";", quote = "\"",
                     dec = ",", fill = TRUE)
-round3 <- read.csv2("round3.csv", header=TRUE, sep = ";", quote = "\"",
+#round3 <- read.csv2("round3.csv", header=TRUE, sep = ";", quote = "\"",
                     dec = ",", fill = TRUE)
-round4 <- read.csv2("round4.csv", header=TRUE, sep = ";", quote = "\"",
+#round4 <- read.csv2("round4.csv", header=TRUE, sep = ";", quote = "\"",
                     dec = ",", fill = TRUE)
-round5 <- read.csv2("round5.csv", header=TRUE, sep = ";", quote = "\"",
+#round5 <- read.csv2("round5.csv", header=TRUE, sep = ";", quote = "\"",
                     dec = ",", fill = TRUE)
 
 ## something went wrong here: when loading the csv-files into R they no longer 
 ## contain the same number of observations and variables as with the originally
 ## downloaded data
+
+# instead: load data from saved Rdata-files;
+
+load("Round5.RData")
+load("Round4.RData")
+load("Round3.RData")
+load("Round2.RData")
+## this time the number of observations and variables stayed the same
