ff913409109e71bc737d88c672aef0686ed42458
Jasmin <jasmincantzler@Jasmins-MacBook-Air.local>
2015-11-12 08:19:19 +0100

    nothing substantial

---
+++ Assignment 3/Data Analysis.R
@@ -0,0 +1,9 @@
+##########################################################
+# Data Analysis                                          #
+# Wiebke Weiger & Jasmin Cantzler                        # 
+# last updated: 12 November 2015                         #
+##########################################################
+
+# Set working directory
+#setwd("C:/Users/Wiebke/Documents/RepRes/TaxMorale/Assignment 3/PreparatoryWork")
+setwd("/Users/jasmincantzler/Documents/TaxMorale/Assignment 3/PreparatoryWork")
