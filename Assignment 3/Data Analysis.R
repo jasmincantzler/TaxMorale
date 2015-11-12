477b7ca71944f6c183d3ed203ae4d7de5b1b6862
Jasmin <jasmincantzler@Jasmins-MacBook-Air.local>
2015-11-12 12:49:25 +0100

    update

--- Assignment 3/Data Analysis.R
+++ Assignment 3/Data Analysis.R
@@ -8,16 +8,27 @@
 #setwd("C:/Users/Wiebke/Documents/RepRes/TaxMorale/Assignment 3/PreparatoryWork")
 setwd("/Users/jasmincantzler/Documents/TaxMorale/Assignment 3/PreparatoryWork")
 
+# Load data
 mydata <- read.csv('Afrobarometer.final.csv')
+
+# Load packages
+library(ggplot2)
 
 # CONDUCTS BASIC DESCRIPTIVE STATISTICS 
 summary(mydata) 
-summary(Afrobarometer.final$TaxMorale)
-table(mydata$TaxMorale)
+summary(mydata$TaxMorale)
 
-install.packages("pastecs")
-library(pastecs)
-stat.desc(mydata)
+barplot(prop.table(table(mydata$TaxMorale)))
+
+#creates a histogram of our dependent variable Tax Morale
+qplot(mydata$TaxMorale,
+      geom="histogram",
+      binwidth=8,
+      main="Summary Statistic",
+      xlab="The tax department always has the right to make people pay taxes.",
+      fill=I("lightblue"))
+
+
 
 # CONDUCTS INFERENTIAL STATISTICS
-Reg1 <- lm(TaxMorale~TrustPresident, data=mydata)
+
