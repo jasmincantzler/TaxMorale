454144e3ccc91d6158a081b7e70191760260285b
wwiebke <w-weiger@msn.com>
2015-12-03 20:15:53 +0100

    some additions

--- Final Project/RegressionModels.R
+++ Final Project/RegressionModels.R
@@ -26,7 +26,13 @@ round5 <- read.csv('round5.final.csv')
 round4 <- read.csv('round4.final.csv')
 round3 <- read.csv('round3.final.csv')
 
-# First step: Sort factor levels (they are currently in alphabetical order) for output to be more easily
+##########################################################################################################
+## Some preparations:
+# First: Sort factor levles
+# Second: Turn "Year" variable into factor
+# Third: Recode dependent variable "TaxMorale" so that it has 3 instead of 5 levels
+
+# First: Sort factor levels (they are currently in alphabetical order) for output to be more easily
 # readable:
 
 # Dependent Variable: tax morale:
@@ -133,17 +139,61 @@ levels(round3$Enforce2) # now order is correct:
 # [1] "Not at all likely" "Not very likely"   "Likely"            "Very Likely"
 
 
+# Second: turn "Year"-variable into factor:
+
+str(data.factors) # shows that Year is currently treated as a numeric variable
+data.factors$Year <- cut(data.factors$Year,
+                              breaks = c(0, 2005, 2008, 2015),
+                              labels = c("2005", "2008", "2015"))
+str(data.factors) # shows that Year is now a factor vairable with 3 levels
+levels(data.factors$Year) # in correct order: 2005, 2008, 2015
 
 
 
-# Models:
+# Third: Recode dependent variable "TaxMorale" so that it has 3 instead of 5 levels
+
+data.3levels <- data.numeric
+
+data.3levels$TaxMorale[data.3levels$TaxMorale==2] <- 1
+data.3levels$TaxMorale[data.3levels$TaxMorale==3] <- 2
+data.3levels$TaxMorale[data.3levels$TaxMorale==4] <- 3
+data.3levels$TaxMorale[data.3levels$TaxMorale==5] <- 3
+
+data.3levels$TaxMorale <- cut(data.3levels$TaxMorale,
+                              breaks = c(0, 1, 2, 3),
+                              labels = c("Disagree", "Neither agree nor disagre", "Agree"))
+
+levels(data.3levels$TaxMorale) 
+
+# end of preparatory work
+########################################################################################################
+
+# MODELS
+
 # 1. Models including data for all three years (only those variables that are available in all rounds)
+
 # Model 1.1: 
 # Tax morale as a function of 
 #   - trust in the government (trying out various versions, e.g. trust in president, trust in parliament, etc)
 #   - level of corruption (trying out various versions, e.g. president corrupt, parliament corrupt, etc.)
 #   - country fixed effects
 #   - year fixed effects
+
+reg1.1 <- polr(TaxMorale ~ TrustPresident + CorruptionPresident + Year + Country, method='logistic', 
+               data=data.factors, Hess = TRUE)
+
+summary(reg1.1) ## 26845 observations deleted due to missingness (that is 25.6% of all observations)
+
+#store table
+(ctable <- coef(summary(reg1)))
+#calculate and store p values
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+## combined table
+(ctable <- cbind(ctable, "p value" = p))
+#FEHLERMELDUNG confidence intervals for the parameter estimates 
+#(ci <- confint(reg1))
+
+#Predicted Probabilities
 
 # Model 1.2 (including socio-economic controls):
 #   - trust in the government (trying out various versions, e.g. trust in president, trust in parliament, etc)
@@ -155,9 +205,29 @@ levels(round3$Enforce2) # now order is correct:
 #   - gender
 #   - religious 
 
+reg1.2 <- polr(TaxMorale ~ TrustPresident + CorruptionPresident +  Year + Country + 
+               LivingConditions + Age + Gender + Religion, method='logistic', 
+               data=data.factors, Hess = TRUE)
+
+summary(reg1.2) ## 27912 observations deleted due to missingness (that is 26.7% of all observations)
+
+
 # Model 1.3 (same as 1.1, but with tax morale coded with only 3 levels - disagree, neither nor, agree)
 
+reg1.3 <- polr(TaxMorale ~ TrustPresident + CorruptionPresident + Year + Country, method='logistic', 
+               data=data.3levels, Hess = TRUE)
+
+summary(reg1.3) ## 26845 observations deleted due to missingness (that is 25.6% of all observations)
+
+
 # Model 1.4 (same as 1.2, but with tax morale coded with only 3 levels - disagree, neither nor, agree)
+
+reg1.4 <- polr(TaxMorale ~ TrustPresident + CorruptionPresident +  Year + Country + 
+               LivingConditions + Age + Gender + Religion, method='logistic', 
+               data=data.3levels, Hess = TRUE)
+
+summary(reg1.4) ## 27884 observations deleted due to missingness (that is 26.7% of all observations)
+
 
 # 2. Sub analyses for single years (with variables not available in all rounds)
 # Model 2.1 (round 5 only):
@@ -167,6 +237,15 @@ levels(round3$Enforce2) # now order is correct:
 #   - perceived frequency of others avoiding taxes
 #   - self-employed
 #   - country fixed effects
+
+reg2.1 <- polr(TaxMorale ~ TrustPresident + TrustTax + CorruptionPresident +  AvoidHowOften + 
+               SelfEmployedTax + Country, 
+               method='logistic', 
+               data=round5, 
+               Hess = TRUE)
+
+summary(reg2.1) ## problem: it says "(35800 observations deleted due to missingness)" which is almost 70%
+                ## of all observations
 
 # Model 2.2 (round 5 only, including socio-economic controls):
 #   - trust in the government (trying out various versions, e.g. trust in president, trust in parliament, etc)
@@ -180,11 +259,28 @@ levels(round3$Enforce2) # now order is correct:
 #   - gender
 #   - religious
 
+reg2.2 <- polr(TaxMorale ~ TrustPresident + TrustTax + CorruptionPresident +  AvoidHowOften + 
+               SelfEmployedTax + Country + LivingConditions + Age + Gender + Religion,
+               method='logistic', 
+               data=round5, 
+               Hess = TRUE)
+
+summary(reg2.2) ## problem: it says "(35955 observations deleted due to missingness)" which is almost 70%
+                ## of all observations
+
+
 # Model 2.3 (round 3 only):
 #   - trust in the government (trying out various versions, e.g. trust in president, trust in parliament, etc)
 #   - level of corruption (trying out various versions, e.g. president corrupt, parliament corrupt, etc.)
 #   - perceived likelihood of being detected
 #   - country fixed effects
+
+reg2.3 <- polr(TaxMorale ~ TrustPresident + CorruptionPresident +  Enforce2 + Country,
+               method='logistic', 
+               data=round3, 
+               Hess = TRUE)
+
+summary(reg2.3) ## 7656 observations deleted due to missingness (that is 30.1% of all observations)
 
 # Model 2.4 (round 3 only, including socio-economic controls):
 #   - trust in the government (trying out various versions, e.g. trust in president, trust in parliament, etc)
@@ -196,5 +292,10 @@ levels(round3$Enforce2) # now order is correct:
 #   - gender
 #   - religious
 
+reg2.4 <- polr(TaxMorale ~ TrustPresident + CorruptionPresident +  Enforce2 + Country + 
+               LivingConditions + Age + Gender + Religion,
+               method='logistic', 
+               data=round3, 
+               Hess = TRUE)
 
-
+summary(reg2.4) ## 7845 observations deleted due to missingness (that is 30.9% of all observations)
